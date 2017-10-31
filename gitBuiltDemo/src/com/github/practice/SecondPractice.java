1e61454f01bcb545ad8b5b6074ebb54174e4e276
DellUser <DellUser@10.0.0.151>
2017-10-31 18:56:43 -0400

    Commited By Arjun

---
+++ gitBuiltDemo/src/com/github/practice/SecondPractice.java
@@ -0,0 +1,11 @@
+package com.github.practice;
+
+public class SecondPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			System.out.println("Second Example to save in local and then in remote repository");
+	}
+
+}
