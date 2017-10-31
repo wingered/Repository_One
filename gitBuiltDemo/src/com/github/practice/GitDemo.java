3bb3d3ff656015a96630acc90bce7c9aacd485a1
DellUser <DellUser@10.0.0.151>
2017-10-31 18:38:44 -0400

    Commited by Arjun

---
+++ gitBuiltDemo/src/com/github/practice/GitDemo.java
@@ -0,0 +1,12 @@
+package com.github.practice;
+
+public class GitDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Practicing GitHub Framework by Arjun");
+	}
+
+}
